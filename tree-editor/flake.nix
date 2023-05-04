a05893c5371d219d447fcc831b17d2be00679590
WhiteBlackGoose <wbg@angouri.org>
2023-05-04 17:05:09 +0200

    wip

--- tree-editor/flake.nix
+++ tree-editor/flake.nix
@@ -10,26 +10,27 @@
 
   outputs = inputs@{ nixpkgs, flake-utils, fenix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in {
-      devShells.${system}.default =
-        pkgs.mkShell {
-          buildInputs = [
-            pkgs.cargo
-            pkgs.rustc
-            pkgs.rust-analyzer
-            pkgs.vscode-extensions.vadimcn.vscode-lldb
-            pkgs.imagemagick
-          ];
-          VSCODE_CODELLDB = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}";
-        };
+      let pkgs = nixpkgs.legacyPackages.${system}; in 
+      {
+        devShells.default =
+          pkgs.mkShell {
+            buildInputs = [
+              pkgs.cargo
+              pkgs.rustc
+              pkgs.rust-analyzer
+              pkgs.vscode-extensions.vadimcn.vscode-lldb
+              pkgs.imagemagick
+            ];
+            VSCODE_CODELLDB = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}";
+          };
 
-      defaultPackage = (pkgs.makeRustPlatform {
-        inherit (fenix.packages.${system}.minimal) cargo rustc;
-      }).buildRustPackage {
-        pname = "tri";
-        version = "0.1.0";
-        src = ./.;
-        cargoSha256 = nixpkgs.lib.fakeSha256;
-      };
+        packages.default = (pkgs.makeRustPlatform {
+          inherit (fenix.packages.${system}.minimal) cargo rustc;
+        }).buildRustPackage {
+          pname = "tri";
+          version = "0.0.1";
+          src = ./.;
+          cargoSha256 = "sha256-3M25zF3TiPAdnmf1rxb+xUdBFEGm/LGVg3Xc1lQn5Pk=";
+        };
     });
 }
