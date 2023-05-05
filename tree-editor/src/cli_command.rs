332ad927f0765fc8daa6bdc0e4c252c7d26dc6b4
WhiteBlackGoose <wbg@angouri.org>
2023-05-05 12:30:03 +0200

    docs extended

--- tree-editor/src/cli_command.rs
+++ tree-editor/src/cli_command.rs
@@ -4,9 +4,10 @@ use clap::{Command, command, arg, value_parser, Arg};
 
 pub fn get_cli_command() -> Command {
     command!()
+        .about("CLI tool to manipulate images, preserving the history of changes. Like git, but for images. Powered by imagemagick.")
         .subcommand(
             Command::new("init")
-                .about("Initialize the TRI metafile in the current folder")
+                .about("Initialize the TRI metafile in the current folder. Will create files starting with `tri-`.")
                 .arg(
                     arg!(
                     -p --path <FILE> "Specify the path to the initial image"
@@ -17,7 +18,7 @@ pub fn get_cli_command() -> Command {
         )
         .subcommand(
             Command::new("commit")
-                .about("Make a commit based on the current one and bump HEAD to it")
+                .about("Make a commit based on the current one and bump HEAD to it.")
                 .arg(
                     arg!(<cmd> ... "magick commands")
                         .trailing_var_arg(true)
@@ -32,7 +33,7 @@ pub fn get_cli_command() -> Command {
         )
         .subcommand(
             Command::new("log")
-                .about("Print history of changes from HEAD to the Root")
+                .about("Print history of changes from HEAD to the Root. The ones on the top are the most recent one. The last line corresponds to root (initial image) and doesn't have commands.")
         )
         .subcommand(
             Command::new("tree")
@@ -40,11 +41,11 @@ pub fn get_cli_command() -> Command {
         )
         .subcommand(
             Command::new("tree-watch")
-                .about("Visualizes the tree of commits")
+                .about("Visualizes the tree of commits interactively. Recommended use: open another terminal, run this command there, and in your first terminal, keep performing commits. The displayed tree will automatically update as you make commits.")
         )
         .subcommand(
             Command::new("reset")
-                .about("Reset HEAD to another commit")
+                .about("Reset HEAD to another commit and regenerate the output file.")
                 .arg(Arg::new("addr"))
                 .arg(
                     arg!(
