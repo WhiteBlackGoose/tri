612453e0a4877c4f5d707dc2410dfe126414fa6c
WhiteBlackGoose <wbg@angouri.org>
2023-05-10 11:55:44 +0200

    added imagemagick as dependency, refactored

--- flake.nix
+++ flake.nix
@@ -34,35 +34,41 @@
           pkgs = nixpkgs.legacyPackages.${system}; in
         {
           default = (nixpkgs.legacyPackages.${system}.pkgs.makeRustPlatform {
-          inherit (fenix.packages.${system}.minimal) cargo rustc;
-        }).buildRustPackage {
-          pname = "tri";
-          version = "0.0.1";
-          src = ./.;
+              inherit (fenix.packages.${system}.minimal) cargo rustc;
+            }).buildRustPackage {
+            pname = "tri";
+            version = "0.0.1";
+            src = ./.;
 
-          nativeBuildInputs = [
-            pkgs.installShellFiles
-          ];
+            nativeBuildInputs = [
+              pkgs.installShellFiles
+              pkgs.makeWrapper
+            ];
 
-          # TODO: add elvish and powershell
-          postInstall = ''
-            installManPage ./artifacts/tri.1
-            installShellCompletion ./artifacts/_tri
-            installShellCompletion ./artifacts/tri.bash
-            installShellCompletion ./artifacts/tri.fish
-          '';
+            buildInputs = [
+              pkgs.imagemagick
+            ];
 
-          # cargoSha256 = "";
-          cargoSha256 = "sha256-i/uT0q81Fhf8OK0oDI4w3zx5W1zS3shCwVvvK/bOxko=";
-          meta = with pkgs.lib; {
-            homepage = "https://github.com/WhiteBlackGoose/tree-imagemagick-editor";
-            description = "Graphic editor with immutable and reproducible changes/transformations, based on imagemagick and inspired by git and nix";
-            platforms = platforms.all;
-            maintainers = with maintainers; [ WhiteBlackGoose ];
-            license = licenses.gpl3Plus;
-            mainProgram = "tri";
+            # TODO: add elvish and powershell
+            postInstall = ''
+              installManPage ./artifacts/tri.1
+              installShellCompletion ./artifacts/_tri
+              installShellCompletion ./artifacts/tri.bash
+              installShellCompletion ./artifacts/tri.fish
+              wrapProgram $out/bin/tri --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.imagemagick ]}
+            '';
+
+            # cargoSha256 = "";
+            cargoSha256 = "sha256-i/uT0q81Fhf8OK0oDI4w3zx5W1zS3shCwVvvK/bOxko=";
+            meta = with pkgs.lib; {
+              homepage = "https://github.com/WhiteBlackGoose/tree-imagemagick-editor";
+              description = "Graphic editor with immutable and reproducible changes/transformations, based on imagemagick and inspired by git and nix";
+              platforms = platforms.all;
+              maintainers = with maintainers; [ WhiteBlackGoose ];
+              license = licenses.gpl3Plus;
+              mainProgram = "tri";
+            };
           };
-        };
       });
     };
 }
