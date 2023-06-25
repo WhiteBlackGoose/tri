975a646cdfadda52ba6c0cc7e54806fae9bd6310
WhiteBlackGoose <wbg@angouri.org>
2023-06-25 09:07:28 +0200

    Magick arguments are now escaped
    Problem: when you pass arguments, which have spaces
    or commas or some other weird stuff in them, e. g.
    
        -annotate 100x100 "hello, world!"
    
    it would be saved as
    
        -annotate 100x100 hello, world
    
    which, of course, ruins the csv.
    
    This fix escapes them as following:
    
        "-annotate" "100x100" "hello, world"

--- src/magick.rs
+++ src/magick.rs
@@ -1,4 +1,7 @@
-use std::{fmt::Display};
+use std::fmt::Display;
+use urlencoding::{encode, decode};
+
+use crate::error::TRIError;
 
 #[derive(Clone)]
 #[derive(Ord, Eq, PartialOrd, PartialEq)]
@@ -7,16 +10,33 @@ pub struct MagickCommand {
 }
 
 impl MagickCommand {
-    pub fn from_string(s: &str) -> MagickCommand {
-        MagickCommand {
-            args: s.split(' ').map(String::from).collect::<Vec<_>>()
+    pub fn decode(s: &str) -> Result<MagickCommand, TRIError> {
+        let decoded = s.split(' ').map(|a| decode(a));
+        if decoded.clone().any(|d| d.is_err()) {
+            return Err(TRIError::MagickDecodingError(String::from(s)));
         }
+        Ok(
+        MagickCommand {
+            args:
+                decoded
+                .map(|v| v.unwrap().into_owned())
+                .collect::<Vec<_>>()
+        }
+        )
+    }
+    pub fn encode(s: &MagickCommand) -> String {
+        s.args
+            .iter()
+            .map(|a| encode(a).into_owned())
+            .collect::<Vec<String>>()
+            .join(" ")
     }
 }
 
 impl Display for MagickCommand {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let str: Vec<&str> = self.args.iter().map(|s| s.as_str()).collect();
+        let strs: Vec<String> = self.args.iter().map(|s| format!("\"{}\"", s)).collect();
+        let str: Vec<&str> = strs.iter().map(|s| s.as_str()).collect();
         f.write_str(str.join(" ").as_str()).unwrap();
         std::fmt::Result::Ok(())
     }
