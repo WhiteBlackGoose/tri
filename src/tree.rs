d42c6b4994eed314c7e9c447f3ef92626e2c4d78
WhiteBlackGoose <wbg@angouri.org>
2023-06-30 19:52:50 +0200

    Template test with FakeIO via DI

--- src/tree.rs
+++ src/tree.rs
@@ -96,3 +96,84 @@ pub fn read_graph(meta: &Meta) -> Result<Node, TRIError> {
         Ok(Node::Image(head.commit))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{hash::Hash, meta::{Meta, CommitKind}, config::Config, io::IO};
+
+    use super::read_graph;
+
+    struct FakeIO {
+        pub f_is_materialized: bool,
+        pub f_meta_exists: bool,
+        pub f_hash: Hash,
+        pub f_meta: Meta,
+        pub f_config_exists: bool,
+        pub f_config: Config
+    }
+    impl super::IO for FakeIO {
+        fn materialize(&mut self, from: &std::path::Path) -> Result<crate::hash::Hash, crate::error::TRIError> {
+            Ok(self.f_hash)
+        }
+
+        fn materialize_magick(&mut self, from: &crate::hash::Hash, cmd: &crate::magick::MagickCommand) -> Result<crate::hash::Hash, crate::error::TRIError>  {
+            Ok(self.f_hash)
+        }
+
+        fn is_materialized(&mut self, hash: &crate::hash::Hash) -> bool {
+            self.f_is_materialized
+        }
+
+        fn expose(&mut self, hash: &crate::hash::Hash, ext: Option<String>) -> Result<(), crate::error::TRIError> {
+            Ok(())
+        }
+
+        fn meta_exists(&mut self) -> bool { self.f_meta_exists }
+
+        fn meta_write(&mut self, meta: &crate::meta::Meta) -> Result<(), crate::error::TRIError> {
+            Ok(())
+        }
+
+        fn meta_read(&mut self) -> Result<crate::meta::Meta, crate::error::TRIError> { Ok(self.f_meta.clone()) }
+
+        fn config_exists(&mut self) -> bool { self.f_config_exists }
+
+        fn config_write(&mut self, config: &crate::config::Config) -> Result<(), crate::error::TRIError> {
+            Ok(())
+        }
+
+        fn config_read(&mut self) -> Result<crate::config::Config, crate::error::TRIError> {
+            Ok(self.f_config.clone())
+        }
+
+        fn list_materialized(&mut self) -> Vec<crate::hash::Hash> {
+            vec![self.f_hash]
+        }
+
+        fn watch_meta<TWatch>(&mut self, watch: TWatch) -> Result<notify::INotifyWatcher, crate::error::TRIError> where TWatch : FnMut(notify::Result<notify::Event>) + Send + 'static {
+            todo!()
+        }
+    }
+    use crate::tree::Node::{Commit, Image};
+    use crate::meta::Line;
+
+    #[test]
+    fn test_read_graph() {
+        let mut io = FakeIO {
+            f_is_materialized: true,
+            f_meta_exists: true,
+            f_config_exists: true,
+            f_hash: Hash::from_string("187329fcf591320b1c0df4e6c832d982").unwrap(),
+            f_config: Config { img_path: String::from("quack") },
+            f_meta: vec![
+                Line { commit: Hash::from_string("187329fcf591320b1c0df4e6c832d982").unwrap(), parent: None, command: None, kind: CommitKind::HEAD }
+                ]
+        };
+        let graph = read_graph(&io.meta_read().unwrap());
+        assert!(graph.is_ok());
+        match graph.unwrap() {
+            Commit(_, _, _) => panic!("Expected image"),
+            Image(_) => ()
+        };
+    }
+}
